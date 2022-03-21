b120e77b767127d56729851af47d1f2e97b2816f
Msi <Msi@DESKTOP-9GKFF1N>
2022-03-21 23:37:28 +0100

    Ejercicios 1 3 de Javier Cejuela Adrián Sánchez

---
+++ eclipse-workspace/Entrega/src/Ejercicio3.java
@@ -0,0 +1,26 @@
+//@Adrián Sánchez de Santos y Javier Cejuela García
+
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Introduzca la cadena de caracteres: ");
+        int num = 0;
+        int suma = 0;
+        String cadena = in.nextLine();
+        for (int i = 0; i < cadena.length(); i++) {
+            char c = cadena.toCharArray()[i];
+            if (Character.isDigit(c)) {
+                num = Character.getNumericValue(c);
+                suma = suma + num;
+            }
+        }
+        if (suma < 20) {
+            System.out.println("La suma de los numeros es: " + suma);
+        }else {
+            RuntimeException miError = new RuntimeException ("Error fuera de rango, la suma es" + suma);
+            throw miError;
+        }
+    }
+}
